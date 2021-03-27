dd59a842266f6474025c80a45251c524bfd62a59
Bruno Kappi <brunokappidematos2@gmail.com>
2021-03-26 23:39:01 -0300

    Setting up Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 //const geocode = require("../../src/utils/geocode") 
 
-console.log('Client side javascript file is loaded!')
+
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -15,13 +15,13 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                messageTwo.textContent = data.forecast 
             }
         })
     })
